863ca96de6af251272e9d9a8cf97939be8e58615
afsanaeva <afsanaeva05@gmail.com>
2024-05-15 11:44:49 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!g</h1>
       <ButtonGradient />
     </>
   );
