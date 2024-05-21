1f4cfcd6d1958194d35008dfbb994313368f4a86
afsanaeva <afsanaeva05@gmail.com>
2024-05-21 12:35:29 +0600

    deploy setting for git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/brainwave/',  // Replace 'repo-name' with your actual repository name
+  base: '/brainwave/',  
   plugins: [react()],
 })
